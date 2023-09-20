1dca318c23af7e07eb0e9953a3fd8275316ae786
quannhg <quan.nguyenhg12@gmail.com>
2023-09-20 01:52:38 -0400

    feat: add deployment, configmap, try rollout, roleback and auto scale

--- labs/3_K8sScaleAndUpdate/app.js
+++ labs/3_K8sScaleAndUpdate/app.js
@@ -4,7 +4,7 @@ var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running!\n')
+  res.send(process.env.MESSAGE + '\n')
 })
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
